2df73b901cc217f50bfe42d40262cad8afea0e9d
Alvaro Alonso Carriazo <alvaroalonso1989@gmail.com>
2012-11-28 19:31:03 +0100

    Comienzo ISIMarket

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ISIMarket::Application.config.secret_token = 'ae61b5ed71e98c646e3d3ca166c6ad2ca35e0bc4bae356a2f9f5240308c2fe03d3afc98ba39bf2cd5c22911102df5cb77474d9c353bfbc18b1d644f8b0335f51'
