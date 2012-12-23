c63e279d0784b4507c249839305672bd3fafb5a1
Bea <bea@ubuntu.ubuntu-domain>
2012-12-23 22:14:50 +0100

    metodos de controlador para las rutas anteriores

---
+++ app/controllers/clientes_controller.rb
@@ -0,0 +1,16 @@
+class ClientesController < ApplicationController
+  def show
+  end
+  def index
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
+end
