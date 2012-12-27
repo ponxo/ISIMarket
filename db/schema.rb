3e94648dcea6872c14dc6ce4a51f67382c79480f
Bea <bea@ubuntu.ubuntu-domain>
2012-12-27 18:53:02 +0100

    vista de index del controlador productosy su metodo

---
+++ db/schema.rb
@@ -0,0 +1,53 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20121226145458) do
+
+  create_table "clientes", :force => true do |t|
+    t.string   "nif"
+    t.string   "nombre"
+    t.string   "password"
+    t.string   "n_cuenta"
+    t.string   "direccion"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "compras", :force => true do |t|
+    t.string   "n_factura"
+    t.float    "coste"
+    t.datetime "fecha"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "productos", :force => true do |t|
+    t.string   "categoria"
+    t.string   "nombre"
+    t.float    "precio_venta"
+    t.float    "precio_compra"
+    t.integer  "stock"
+    t.text     "descripcion"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  create_table "recetas", :force => true do |t|
+    t.string   "titulo"
+    t.text     "preparacion"
+    t.integer  "puntuacion"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+end
