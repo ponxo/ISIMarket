37a13e20d4fba6a5f5dfc3b9c2d490e3e2baeec1
Bea <bea@ubuntu.ubuntu-domain>
2013-01-05 19:03:27 +0100

    metodos de recetas

--- app/controllers/recetas_controller.rb
+++ app/controllers/recetas_controller.rb
@@ -1,17 +1,39 @@
 class RecetasController < ApplicationController
   def show
+    @receta = Receta.find_by_id(params[:id])
+    if not @receta then
+      flash[:warning]= 'Receta no encontrada'
+      redirect_to recetas_path
+    end
   end
   def index
     @recetas = Receta.all
   end
   def new
+    # solo disponible para modo cliente
   end
   def create
+    @receta = Receta.create!(params[:producto])
+    flash[:notice] = "#{@receta.titulo} creada."
+    redirect_to receta_path(@receta)
+    # solo disponible para modo cliente
   end
-  def edit
-  end
-  def update
-  end
+  #def edit
+    #@receta = Receta.find params[:id]
+    # A lo mejor no deberiamos implementar este metodo! o solo para el cliente que la crea
+  #end
+  #def update
+    #@receta = Receta.find params[:id]
+    #@receta.update_attributes!(params[:prodcuto])
+    #flash[:notice] = "#{@receta.titulo} Acualizada."
+    #redirect_to receta_path(@receta)
+    # A lo mejor no deberiamos implementar este metodo! o solo para el cliente que la crea
+  #end
   def destroy
+    @receta = Receta.find(params[:id])
+    @receta.destroy
+    flash[:notice] = "#{@receta.titulo} eliminada."
+    redirect_to recetas_path
+    # solo disponible para modo cliente (el q la crea)
   end
 end
